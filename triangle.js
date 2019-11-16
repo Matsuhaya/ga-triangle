a54520f6f5bf2716f80f4bd6f485ba644bbb8fa2
matsuhaya <matsuhaya007@gmail.com>
2019-11-16 22:32:51 +0900

    ３辺の長さを指定して三角形を生成する処理をコメントアウト
    「面積が最大になる問題は、三角形の位置を問わない」ため、３辺の長さを指定して生成する問題を解くのにGAが適していないと考えた
    なぜなら、評価は面積なのに対し、選択されるデータは座標の位置だからである

--- triangle.js
+++ triangle.js
@@ -64,22 +64,26 @@ export default class Triangle {
   // 三角形の頂点の位置を決める
   static makeAllVertexes = () => {
     let vertexes = [];
-    let sum_sides = $('#input_sum_sides').val();
-
-    if (!sum_sides) {
-      // 3つの頂点をランダムに生成
-      for (let i = 0; i < 3; i++) {
-        vertexes.push(this.makeRandVertex());
-      }
-    } else {
-      // 頂点A(x1,x2)：点Aをランダム生成
-      let A = this.makeRandVertex();
-      let [B, C] = this.makeTwoVertexes(A, sum_sides);
-
-      vertexes.push(A);
-      vertexes.push(B);
-      vertexes.push(C);
+    // 3つの頂点をランダムに生成
+    for (let i = 0; i < 3; i++) {
+      vertexes.push(this.makeRandVertex());
     }
+
+    // let sum_sides = $('#input_sum_sides').val();
+    // if (!sum_sides) {
+    //   // 3つの頂点をランダムに生成
+    //   for (let i = 0; i < 3; i++) {
+    //     vertexes.push(this.makeRandVertex());
+    //   }
+    // } else {
+    //   // 頂点A(x1,x2)：点Aをランダム生成
+    //   let A = this.makeRandVertex();
+    //   let [B, C] = this.makeTwoVertexes(A, sum_sides);
+
+    //   vertexes.push(A);
+    //   vertexes.push(B);
+    //   vertexes.push(C);
+    // }
     return vertexes;
   }
 
@@ -88,8 +92,8 @@ export default class Triangle {
    * @return {array} vertex [x,y]
    **/
   static makeRandVertex() {
-    let x = Math.random() * WIDTH;
-    let y = Math.random() * HEIGHT;
+    let x = Math.floor(Math.random() * WIDTH);
+    let y = Math.floor(Math.random() * HEIGHT);
     return [x, y];
   }
 
@@ -104,86 +108,85 @@ export default class Triangle {
    * @param {Number} sum_sides
    * @return {Number} side
    **/
-  static makeTwoVertexes(A, sum_sides) {
-    let limit_side = sum_sides / 2;
-    let init = true;
+  // static makeTwoVertexes(A, sum_sides) {
+  //   let limit_side = sum_sides / 2;
+  //   let init = true;
 
-    // 2つの円が2点で交わるようなr1,r2の生成
-    let r1, r2, B, C;
-    const makeTwoCircle = () => {
+  //   // 2つの円が2点で交わるようなr1,r2の生成
+  //   let r1, r2, B, C;
+  //   const makeTwoCircle = () => {
 
-      // x2とy2は点Aからの相対座標
-      let d = Math.random() * limit_side;
-      let _θ = Math.random() * 2 * Math.PI;
-      let x1 = d * Math.cos(_θ);
-      let y1 = d * Math.sin(_θ);
+  //     // x2とy2は点Aからの相対座標
+  //     let d = Math.random() * limit_side;
+  //     let _θ = Math.random() * 2 * Math.PI;
+  //     let x1 = d * Math.cos(_θ);
+  //     let y1 = d * Math.sin(_θ);
 
-      do {
-        // 点Aを中心とするr1を決定
-        r1 = Math.random() * limit_side;
-        // 点Bを中心とするr2を決定
-        r2 = sum_sides - d - r1;
-      }
-      while (d < Math.abs(r1 - r2) || d > Math.abs(r1 + r2));
+  //     do {
+  //       // 点Aを中心とするr1を決定
+  //       r1 = Math.random() * limit_side;
+  //       // 点Bを中心とするr2を決定
+  //       r2 = sum_sides - d - r1;
+  //     }
+  //     while (d < Math.abs(r1 - r2) || d > Math.abs(r1 + r2));
 
-      // 点Bの座標
-      // B = [x1, y1]
-      B = [x1 + A[0], y1 + A[1]];
-      // 点Bがキャンバス内に存在しない場合に実行する再帰関数
-      if (B[0] < 0 || B[0] > WIDTH || B[1] < 0 || B[1] > HEIGHT) {
-        return makeTwoCircle();
-      }
+  //     // 点Bの座標
+  //     // B = [x1, y1]
+  //     B = [x1 + A[0], y1 + A[1]];
+  //     // 点Bがキャンバス内に存在しない場合に実行する再帰関数
+  //     if (B[0] < 0 || B[0] > WIDTH || B[1] < 0 || B[1] > HEIGHT) {
+  //       return makeTwoCircle();
+  //     }
 
+  //     let a = (x1 ** 2 + y1 ** 2 + r1 ** 2 - r2 ** 2) / 2
+  //     let x = (a * x1 + y1 * Math.sqrt((x1 ** 2 + y1 ** 2) * r1 ** 2 - a ** 2)) / (x1 ** 2 + y1 ** 2);
+  //     let y = (a * y1 - x1 * Math.sqrt((x1 ** 2 + y1 ** 2) * r1 ** 2 - a ** 2)) / (x1 ** 2 + y1 ** 2);
+  //     // 点Cの座標
+  //     C = [x + A[0], y + A[1]];
 
-      let a = (x1 ** 2 + y1 ** 2 + r1 ** 2 - r2 ** 2) / 2
-      let x = (a * x1 + y1 * Math.sqrt((x1 ** 2 + y1 ** 2) * r1 ** 2 - a ** 2)) / (x1 ** 2 + y1 ** 2);
-      let y = (a * y1 - x1 * Math.sqrt((x1 ** 2 + y1 ** 2) * r1 ** 2 - a ** 2)) / (x1 ** 2 + y1 ** 2);
-      // 点Cの座標
-      C = [x + A[0], y + A[1]];
+  //     console.log(`x+A[0]:${x + A[0]}`, `y + A[1]:${y + A[1]}`);
+  //     // x,yの交点の内１つ目がキャンバス内に存在しない場合に実行する
+  //     if (C[0] < 0 || C[0] > WIDTH || C[1] < 0 || C[1] > HEIGHT) {
+  //       console.log("x,y are out(1)");
+  //       console.log({ A }, { B }, { C });
+  //       x = (a * x1 - y1 * Math.sqrt((x1 ** 2 + y1 ** 2) * r1 ** 2 - a ** 2)) / (x1 ** 2 + y1 ** 2);
+  //       y = (a * y1 + x1 * Math.sqrt((x1 ** 2 + y1 ** 2) * r1 ** 2 - a ** 2)) / (x1 ** 2 + y1 ** 2);
+  //       C = [x + A[0], y + A[1]];
+  //     }
 
-      console.log(`x+A[0]:${x + A[0]}`, `y + A[1]:${y + A[1]}`);
-      // x,yの交点の内１つ目がキャンバス内に存在しない場合に実行する
-      if (C[0] < 0 || C[0] > WIDTH || C[1] < 0 || C[1] > HEIGHT) {
-        console.log("x,y are out(1)");
-        console.log({ A }, { B }, { C });
-        x = (a * x1 - y1 * Math.sqrt((x1 ** 2 + y1 ** 2) * r1 ** 2 - a ** 2)) / (x1 ** 2 + y1 ** 2);
-        y = (a * y1 + x1 * Math.sqrt((x1 ** 2 + y1 ** 2) * r1 ** 2 - a ** 2)) / (x1 ** 2 + y1 ** 2);
-        C = [x + A[0], y + A[1]];
-      }
+  //     console.log(`x+A[0]:${x + A[0]}`, `y + A[1]:${y + A[1]}`);
+  //     // x,yの交点の内2つ目がキャンバス内に存在しない場合に実行する再帰関数
+  //     if (C[0] < 0 || C[0] > WIDTH || C[1] < 0 || C[1] > HEIGHT) {
+  //       console.log("x,y are out(2)");
+  //       console.log({ A }, { B }, { C });
+  //       return makeTwoCircle();
+  //     }
+  //     console.log({ C })
+  //   }
 
-      console.log(`x+A[0]:${x + A[0]}`, `y + A[1]:${y + A[1]}`);
-      // x,yの交点の内2つ目がキャンバス内に存在しない場合に実行する再帰関数
-      if (C[0] < 0 || C[0] > WIDTH || C[1] < 0 || C[1] > HEIGHT) {
-        console.log("x,y are out(2)");
-        console.log({ A }, { B }, { C });
-        return makeTwoCircle();
-      }
-      console.log({ C })
-    }
+  //   if (init) {
+  //     init = false;
+  //     makeTwoCircle();
+  //   }
 
-    if (init) {
-      init = false;
-      makeTwoCircle();
-    }
+  //   // 2つの円をキャンバス描画
+  //   if (canvas.getContext) {
+  //     var ctx = canvas.getContext("2d");
+  //     ctx.beginPath();
+  //     ctx.arc(A[0], HEIGHT - A[1], r1, 0 * Math.PI / 180, 360 * Math.PI / 180, false);
+  //     ctx.strokeStyle = "purple";
+  //     ctx.lineWidth = 2;
+  //     ctx.stroke();
 
-    // 2つの円をキャンバス描画
-    // if (canvas.getContext) {
-    //   var ctx = canvas.getContext("2d");
-    //   ctx.beginPath();
-    //   ctx.arc(A[0], HEIGHT - A[1], r1, 0 * Math.PI / 180, 360 * Math.PI / 180, false);
-    //   ctx.strokeStyle = "purple";
-    //   ctx.lineWidth = 2;
-    //   ctx.stroke();
+  //     ctx.beginPath();
+  //     ctx.arc(B[0], HEIGHT - B[1], r2, 0 * Math.PI / 180, 360 * Math.PI / 180, false);
+  //     ctx.strokeStyle = "purple";
+  //     ctx.lineWidth = 2;
+  //     ctx.stroke();
+  //   }
+  //   console.log("Let's drowing")
+  //   console.log({ A }, { B }, { C });
 
-    //   ctx.beginPath();
-    //   ctx.arc(B[0], HEIGHT - B[1], r2, 0 * Math.PI / 180, 360 * Math.PI / 180, false);
-    //   ctx.strokeStyle = "purple";
-    //   ctx.lineWidth = 2;
-    //   ctx.stroke();
-    // }
-    console.log("Let's drowing")
-    console.log({ A }, { B }, { C });
-
-    return [B, C];
-  }
+  //   return [B, C];
+  // }
 }
